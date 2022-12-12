7429d82588e7cb480f79b9391afe92ddbcd71bb8
visurm76 <visurm76@gmail.com>
2022-12-12 13:32:02 +0300

    Расширение шаблона на основе базового шаблона

---
+++ hello/firstapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+# Create your views here.
+from django.template.response import TemplateResponse
+
+"""
+def index(request):    
+    return HttpResponse("Hello word! My first project django")
+"""
+"""__________Статичные файлы_______________________"""
+
+def index(request):
+    return render(request, "firstapp/index.html")
