e590d0663b2295c6d261054e8c7650f53562bd93
visurm76 <visurm76@gmail.com>
2022-12-13 11:03:23 +0300

    Change commit double forms forms
    forms

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from .forms import UserForm
 
 # Create your views here.
 from django.template.response import TemplateResponse
@@ -12,8 +13,9 @@ def index(request):
 """__________Статичные файлы_______________________"""
 
 def index(request):
+    userform = UserForm()
     cat = ["Ноутбук", "Принтеры", "Сканеры"]
-    return render(request, "firstapp/index.html", context={"cat": cat})
+    return render(request, "firstapp/index.html", {"form":userform})#context={"cat": cat})
 
 def about(request):
     return render(request, "firstapp/about.html")
