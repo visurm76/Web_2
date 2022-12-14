6111c9487703da8d28f1124a7b856907a7ac61d4
visurm76 <visurm76@gmail.com>
2022-12-14 09:35:36 +0300

    GET and POST requests

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -3,19 +3,15 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from .forms import UserForm
 
-# Create your views here.
-from django.template.response import TemplateResponse
-
-"""
-def index(request):    
-    return HttpResponse("Hello word! My first project django")
-"""
-"""__________Статичные файлы_______________________"""
-
 def index(request):
-    userform = UserForm()
-    cat = ["Ноутбук", "Принтеры", "Сканеры"]
-    return render(request, "firstapp/index.html", {"form":userform})#context={"cat": cat})
+    if request.method == "POST":
+        name = request.POST.get ("name") # получить значение поля Имя
+        age = request.POST.get("age") # получить значение поля Возраст
+        output = "<h2>Пользователь</h2><h3>Имя - {О},Возраст - {l}</hЗ>".format(name, age)
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request,"firstapp/index.html", {"form": userform})
 
 def about(request):
     return render(request, "firstapp/about.html")
