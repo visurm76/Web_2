f9feb67d6592a843dd210f55f4b0c52c9a2a8eda
visurm76 <visurm76@gmail.com>
2022-12-15 11:09:40 +0300

    Read and write informations to base

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
-class UserForm (forms.Form):
-    name = forms.CharField(label="Имя миента",widget=forms.TextInput(attrs={"class":"myfield"}))
-    age = forms.IntegerField(label="Boзpacт клиента", widget=forms.NumberInput(attrs={"class":"myfield"}))
+class UserForm(forms.Form):
+    name = forms.CharField(label="Имя клиента", min_length=3)
+    age = forms.IntegerField(label="Boзpacт клиента", min_value=1, max_value=100)
--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
+    objects = models.Manager()
 
